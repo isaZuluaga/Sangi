6f1def38a4529627d261cd923dd4a9d8ed2ba173
izuluagag1998 <izuluagag1998@gmail.com>
2020-04-10 17:09:47 -0500

    initial react routing

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ObjectList from './ObjectList';
-import ObjectInfo from './ObjectInfo'
-import UserInfo from './UserInfo'
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
